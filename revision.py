dc2f9131b829ff789c6262679fdc1e65eb567ef7
AngelaSophia <ayugisophiaangela@gmail.com>
2021-05-22 21:21:45 +0300

    happy

---
+++ revision.py
@@ -0,0 +1,45 @@
+first_name="ayugi"
+last_name="sophia"
+school="akirachix"
+country="kenya"
+year_of_birth=1997
+
+full_name=first_name+last_name
+print(full_name)
+print(full_name.upper())
+print(school.upper())
+age=2021-year_of_birth
+print(age)
+print(f"Hello, my fullname is {full_name},I am currently studing at {school} \n my nationality is {country} am {age} years old")
+print("Hello, my name is {} and am taking my studies at {} \n am from this country{} finaly am {} years old".format(full_name,school,country,age))
+
+name="lisa lab"
+print(name.title())
+first_name="laura"
+last_name="betty"
+full_name=f"{first_name} {last_name}"
+print(full_name)
+print(f"Hello, {full_name.title()}!")
+# use of f-string to compose a massage
+massage=f"Hello,{full_name.title()}!"
+print(massage)
+# adding whitespace to Strings with tabs or new lines
+fevorite_language="Python"
+print(fevorite_language.rstrip())
+sentence="I love my life and my country"
+print(sentence.rstrip())
+name="Brawlian,"
+massage="Hello,would you like to do some Python today?"
+sentence=f"{name}  {massage}"
+print(sentence)
+var="Brawlian"
+print(var.upper())
+print(var.lower())
+print(var.title())
+Author="Michel"
+Quote=" said be strong,don't be afraid,be focused "
+output=f"{Author} {Quote}"
+print(output)
+
+
+
